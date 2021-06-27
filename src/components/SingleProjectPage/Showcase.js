86894e7bb3b0371b6ca419a096e15d7f02210884
terryzzzzz <terry@voyanceinsights.com.au>
2021-06-28 07:40:42 +1000

    Update bg image attchment

--- src/components/SingleProjectPage/Showcase.js
+++ src/components/SingleProjectPage/Showcase.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import { BackgroundImage } from "react-image-and-background-image-fade"
+// import { BackgroundImage } from "react-image-and-background-image-fade"
 
 export default function Showcase({ showcaseImage }) {
     return (
         <div
-            className="w-full h-auto lg:h-screen"
+            className="w-full h-auto lg:h-screen bg-fixed bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${showcaseImage.url})` }}
         >
-            <BackgroundImage
+            {/* <BackgroundImage
                 src={showcaseImage.url}
                 width="100%"
                 height="100%"
                 lazyLoad
                 className="bg-fixed bg-cover bg-center bg-no-repeat"
-            >
-                <div className="py-80 lg:py-0"></div>
-            </BackgroundImage>
+            > */}
+            <div className="py-80 lg:py-0"></div>
+            {/* </BackgroundImage> */}
         </div>
     )
 }

cab1a59ca795bc7cf0f4c32e7b0dbf37683e145e
terryzzzzz <terry@voyanceinsights.com.au>
2021-06-28 07:52:15 +1000

    Fix backround image bug

--- src/components/SingleProjectPage/Showcase.js
+++ src/components/SingleProjectPage/Showcase.js
@@ -1,20 +1,20 @@
 import React from 'react'
-// import { BackgroundImage } from "react-image-and-background-image-fade"
+import { BackgroundImage } from "react-image-and-background-image-fade"
 
 export default function Showcase({ showcaseImage }) {
     return (
         <div
-            className="w-full h-auto lg:h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${showcaseImage.url})` }}
+            className="w-full h-auto lg:h-screen"
         >
-            {/* <BackgroundImage
+            <BackgroundImage
                 src={showcaseImage.url}
                 width="100%"
                 height="100%"
                 lazyLoad
-                className="bg-fixed bg-cover bg-center bg-no-repeat"
-            > */}
-            <div className="py-80 lg:py-0"></div>
-            {/* </BackgroundImage> */}
+                className="lg:bg-fixed bg-cover bg-center bg-no-repeat"
+            >
+                <div className="py-80 lg:py-0"></div>
+            </BackgroundImage>
         </div>
     )
 }
